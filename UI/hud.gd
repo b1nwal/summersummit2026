27587c4fafdfadae81bb59dadcc4c9f8545de3fa
panyongpin <handragon11@gmail.com>
2026-09-05 10:12:39 -0700

    minor hud updates

--- UI/hud.gd
+++ UI/hud.gd
@@ -2,3 +2,6 @@ extends CanvasLayer
 
 func update_timer(time):
 	$TimeLabel.text = str(time)
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
--- main.gd
+++ main.gd
@@ -3,14 +3,15 @@ extends Node2D
 var playerPast_scene = preload("res://Players/playerPast.tscn")
 var brian_scene = preload("res://gameElements/tempartifact.tscn")
 
-
+var score
 var time
 
 func _ready():
 	randomize()
-	$player.start($playerspawns/playerspawn.position);
 	print("beginning")
-	time = 0
+	$player.start($playerspawns/playerspawn.position);
+	time = 60
+	score = 0
 	var playerPast = playerPast_scene.instantiate()
 	add_child(playerPast)
 	playerPast.position = Vector2(500,1000)
@@ -29,6 +30,7 @@ func _ready():
 	new_round()
 
 func new_round():
+	time = 60
 	var random_int = randi_range(1, 3)
 	if random_int == 1:
 		$player.position = $playerspawns/playerspawn.position
@@ -36,11 +38,15 @@ func new_round():
 		$player.position = $playerspawns/playerspawn2.position
 	if random_int == 3:
 		$player.position = $playerspawns/playerspawn3.position
+	
+	
+	
 	$RoundTimer.start()
 	$HUD.update_timer(time)
+	$HUD.update_score(score)
 
 func _on_round_timer_timeout():
-	time += 1
+	time -= 1
 	$RoundTimer.start()
 	$HUD.update_timer(time)
 
