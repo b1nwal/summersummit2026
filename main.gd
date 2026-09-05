29727b251fe1870d42c36842cc54b61134e9d968
brahm <brahmwongwandanee@gmail.com>
2026-09-05 08:35:30 -0700

    refactored add_artifact

--- main.gd
+++ main.gd
@@ -1,9 +1,7 @@
 extends Node2D
 
 var playerPast_scene = preload("res://Players/playerPast.tscn")
-var brian_scene = preload("res://gameElements/tempartifact.tscn")
-
-
+var artifact_scene = preload("res://gameElements/tempartifact.tscn")
 var time
 
 func _ready():
@@ -15,29 +13,33 @@ func _ready():
 	add_child(playerPast)
 	playerPast.position = Vector2(500,1000)
 	#spawn brians
-	var brian1 = brian_scene.instantiate()
-	add_child(brian1)
-	brian1.position = Vector2(500,1000)
-	
-	var brian2 = brian_scene.instantiate()
-	add_child(brian2)
-	brian2.position = Vector2(1900,1750)
-
-	var brian3 = brian_scene.instantiate()
-	add_child(brian3)
-	brian3.position = Vector2(3500,2050)
+	add_artifact(Vector2(500,1000))
+	add_artifact(Vector2(1900,1750))
+	add_artifact(Vector2(3500,2050))
 	new_round()
 
 func new_round():
-	var random_int = randi_range(1, 3)
+	var random_int = randi_range(1, 6)
 	if random_int == 1:
 		$player.position = $playerspawns/playerspawn.position
 	if random_int == 2:
 		$player.position = $playerspawns/playerspawn2.position
 	if random_int == 3:
 		$player.position = $playerspawns/playerspawn3.position
+	if random_int == 4:
+		$player.position = $playerspawns/playerspawn4.position
+	if random_int == 5:
+		$player.position = $playerspawns/playerspawn5.position
+	if random_int == 6:
+		$player.position = $playerspawns/playerspawn6.position
 	$RoundTimer.start()
 	$HUD.update_timer(time)
+
+func add_artifact(position: Vector2):
+	var artifact = artifact_scene.instantiate()
+	artifact.position = position
+	add_child(artifact)
+	
 
 func _on_round_timer_timeout():
 	time += 1
