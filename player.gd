65c6faba94d2d2ec38557a2ba902c86ca4ebf584
panyongpin <handragon11@gmail.com>
2026-09-05 00:38:35 -0700

    flashlight

--- player.gd
+++ player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-var speed = get_meta("speed")
+@onready var speed = get_meta("speed")
+
+var facing := Vector2.RIGHT
+
+func _physics_process(delta: float) -> void:
+	var input_dir := Input.get_vector("move_left","move_right","move_up","move_down")
+	if input_dir != Vector2.ZERO:
+		facing = input_dir
+	velocity = input_dir * speed
+	move_and_slide()
+
+	$FlashLight.rotation = facing.angle() + (PI/4)
 
 func start(pos: Vector2):
 	position = pos
 	show()
-	
-func _process(delta: float) -> void:
-	# movement
-	velocity = Input.get_vector("move_left","move_right","move_up","move_down") * speed
-	move_and_slide()
