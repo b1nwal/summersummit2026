3d9df6c10146305a3b741294da7dfa8d2b45d0ed
Reilley Pfrimmer <reilley.pfrimmer@gmail.com>
2026-09-06 01:08:45 -0700

    Fixed camera lerping, no longer lags behind. Fixed broken occlusion. Added probably unused textures for the artefact stand.

--- UI/camera.gd
+++ UI/camera.gd
@@ -1,10 +1,15 @@
 extends Camera2D
 
-@onready var target = get_parent() # Drag your player or target here
-var lerp_speed: float = 5
+@onready var target = get_parent()
+var lerp_speed := 5.0
+var lead_distance := 100.0
 
 func _process(delta: float) -> void:
 	if not target:
 		return
-	var target_pos = target.global_position
+
+	var vel = target.velocity  # or whatever your player uses
+	var forward = vel.normalized()
+	var target_pos = target.global_position + forward * lead_distance
+
 	global_position = global_position.lerp(target_pos, lerp_speed * delta)
