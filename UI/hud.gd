762012ed81a9e4e0b701549500d55a539aff1192
Reilley <49217120+b1nwal@users.noreply.github.com>
2026-09-05 17:16:31 -0700

    Revert "tile mapping. player animations. HUD fixes. ummmm good luck"

--- UI/hud.gd
+++ UI/hud.gd
@@ -2,12 +2,6 @@ extends CanvasLayer
 
 func update_timer(time):
 	$TimeLabel.text = str(time)
-	if time <= 15:
-		$TimeLabel.add_theme_color_override("font_color", Color(255, 0, 0))
-		$TimeLabel.add_theme_color_override("font_outline_color", Color(255, 255, 255))
-	elif time > 15:
-		$TimeLabel.add_theme_color_override("font_color", Color(255, 255, 255))
-		$TimeLabel.add_theme_color_override("font_outline_color", Color(0, 0, 0))
 
 func update_score(score):
-	$MarginContainer/ScoreLabel.text = str(score)
+	$ScoreLabel.text = str(score)
