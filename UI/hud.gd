38f60ca7baa9427be6378ea47ac76a03f99e6603
panyongpin <handragon11@gmail.com>
2026-09-06 09:03:48 -0700

    Waypoint added

--- UI/hud.gd
+++ UI/hud.gd
@@ -1,5 +1,59 @@
 extends CanvasLayer
 
+@onready var waypoint := $Waypoint
+@onready var waypointBL := $Waypoint/bottomleft
+@onready var waypointBR := $Waypoint/bottomright
+@onready var waypointB := $Waypoint/bottom
+@onready var waypointL := $Waypoint/left
+@onready var waypointR := $Waypoint/right
+@onready var waypointTL := $Waypoint/topleft
+@onready var waypointTR := $Waypoint/topright
+@onready var waypointT := $Waypoint/top
+
+const top = 96.0
+const left = 96.0
+const right = 1825.0
+const bottom = 984.0
+const WAYPOINT_MARGIN := 96.0
+var waypoint_target = null
+
+func _process(delta: float) -> void:
+	if waypoint_target == null:
+		waypoint.hide()
+		return
+	
+	var screen_pos: Vector2 = get_viewport().get_canvas_transform() * waypoint_target
+	var safe := get_viewport().get_visible_rect().grow(-WAYPOINT_MARGIN)
+	var center = safe.get_center()
+	 
+	if safe.has_point(screen_pos):
+		waypoint.hide()
+		return
+	
+	waypoint.show()
+	waypoint.position = screen_pos.clamp(safe.position, safe.end)
+	
+	for s in waypoint.get_children():
+		s.hide()
+	if waypoint.position.x < left + WAYPOINT_MARGIN and waypoint.position.y < top + WAYPOINT_MARGIN:
+		waypointTL.show()
+	elif waypoint.position.x > right - WAYPOINT_MARGIN and waypoint.position.y < top + WAYPOINT_MARGIN:
+		waypointTR.show()
+	elif waypoint.position.x > right - WAYPOINT_MARGIN and waypoint.position.y > bottom - WAYPOINT_MARGIN:
+		waypointBR.show()
+	elif waypoint.position.x < left + WAYPOINT_MARGIN and waypoint.position.y > bottom - WAYPOINT_MARGIN:
+		waypointBL.show()
+	elif waypoint.position.x == left:
+		waypointL.show()
+	elif waypoint.position.x == right:
+		waypointR.show()
+	elif waypoint.position.y == top:
+		waypointT.show()
+	elif waypoint.position.y == bottom:
+		waypointB.show()
+		
+	print(waypoint.position)
+
 func update_ready(message):
 	$CountDownLabel.text = message
 	if message.contains("Space"):
@@ -31,3 +85,6 @@ func update_objective(objective):
 		$ObjectiveLabel2.text = "Avoid your past selves."
 	else:
 		$ObjectiveLabel.text = "the objective label is cooked what did you do"
+
+func set_waypoint(pos) -> void:
+	waypoint_target = pos
